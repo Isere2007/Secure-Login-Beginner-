e863a23e85b8f3acb02e1ff6e2c3fea55a0f316f
Isere2007 <106511216+Isere2007@users.noreply.github.com>
2023-02-21 02:03:23 -0500

    Add files via upload

---
+++ Secure login.py
@@ -0,0 +1,12 @@
+print("SECURE LOGIN")
+username = input("Enter username: ")
+password = input("Enter password: ")
+if username == "Brandon":
+    print("Welcome Brandon!")
+else:
+    print("Invalid username")
+    if password == "Oreo2007":
+        print("Success!")
+    else:
+        print("Invalid password")
+
